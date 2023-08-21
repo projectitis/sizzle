b79ce47bf68fdb94ad32536b5d98b5e9c19b1cc7
projectitis <peter@projectitis.com>
2023-08-21 13:18:06 +1200

    Added safe size. Performance.

--- lib/src/display/ninegrid.dart
+++ lib/src/display/ninegrid.dart
@@ -41,14 +41,14 @@ class NineGridRepeat {
   NineGridRepeatType centerH;
   NineGridRepeatType centerV;
 
-  static NineGridRepeat all(NineGridRepeatType repeat) {
+  static NineGridRepeat all(NineGridRepeatType type) {
     return NineGridRepeat(
-      left: repeat,
-      top: repeat,
-      right: repeat,
-      bottom: repeat,
-      centerH: repeat,
-      centerV: repeat,
+      left: type,
+      top: type,
+      right: type,
+      bottom: type,
+      centerH: type,
+      centerV: type,
     );
   }
 
@@ -71,6 +71,10 @@ class NineGridRepeat {
       right: NineGridRepeatType.repeat,
       bottom: NineGridRepeatType.repeat,
     );
+  }
+
+  bool isAll(NineGridRepeatType type) {
+    return (left == type && top == type && right == type && bottom == type && centerH == type && centerV == type);
   }
 }
 
@@ -103,21 +107,25 @@ class NineGridComponent extends PositionComponent {
     Vector2 size, {
     NineGridSize? grid,
     NineGridRepeat? repeat,
+    bool useSafeSize = false,
   }) : _image = image {
     this.grid = grid ?? NineGridSize.all(0.0);
     this.repeat = repeat ?? NineGridRepeat.all(NineGridRepeatType.stretch);
+    _safeSize = useSafeSize;
     this.size = size;
   }
 
   Image _image;
   late NineGridSize _grid;
   late NineGridRepeat repeat;
+  bool _safeSize = false;
   bool _needsComposing = true;
   bool _isComposing = false;
   Image? _output;
   BlendMode blendMode = BlendMode.srcOver;
   bool isAntiAlias = true;
   Rect _sizeRect = Rect.zero;
+  Stopwatch stopWatch = Stopwatch();
 
   /// Set the size of the nine-grid
   set grid(NineGridSize g) {
@@ -155,11 +163,30 @@ class NineGridComponent extends PositionComponent {
 
   @override
   set size(Vector2 s) {
+    if (_safeSize) {
+      double mw = _grid.left + _grid.right;
+      double mh = _grid.top + _grid.bottom;
+      double cw = _image.width - mw;
+      double ch = _image.height - mh;
+      s.setValues(
+        (max(0, s.x - mw) / cw).ceil() * cw + mw,
+        (max(0, s.y - mh) / ch).ceil() * ch + mh,
+      );
+    }
     if (size.x != s.x || size.y != s.y) {
       size.setFrom(s);
       _needsComposing = true;
       _sizeRect = Rect.fromLTWH(0, 0, size.x, size.y);
     }
+  }
+
+  /// Will set the size of the image at the specified size or larger,
+  /// ensuring that the repeated areas repeat cleanly in whole numbers.
+  set sizeSafe(Vector2 s) {
+    bool temp = _safeSize;
+    _safeSize = true;
+    size = s;
+    _safeSize = temp;
   }
 
   /// Compose the final image. Will be called by first render call
@@ -173,99 +200,97 @@ class NineGridComponent extends PositionComponent {
     final canvas = Canvas(recorder);
     final paint = Paint()..isAntiAlias = isAntiAlias;
 
-    late Rect src;
-    late Rect dst;
     final Rect srcCenter = Rect.fromLTWH(
       _grid.left,
       _grid.top,
       _image.width - _grid.left - _grid.right,
       _image.height - _grid.top - _grid.bottom,
     );
-    final Rect dstCenter = Rect.fromLTWH(
-      _grid.left,
-      _grid.top,
-      max(0, size.x - _grid.left - _grid.right),
-      max(0, size.y - _grid.top - _grid.bottom),
-    );
 
-    // Top left corner
-    src = Rect.fromLTWH(0, 0, _grid.left, _grid.top);
-    _composeStretch(canvas, src, src, paint);
+    if (repeat.isAll(NineGridRepeatType.stretch)) {
+      // drawImageNine is 2.5x faster
+      canvas.drawImageNine(_image, srcCenter, _sizeRect, paint);
+    } else {
+      stopWatch.start();
+      // Draw manually
+      final Rect dstCenter = Rect.fromLTWH(
+        _grid.left,
+        _grid.top,
+        max(0, size.x - _grid.left - _grid.right),
+        max(0, size.y - _grid.top - _grid.bottom),
+      );
 
-    // Top
-    src = Rect.fromLTWH(_grid.left, 0, srcCenter.width, _grid.top);
-    dst = Rect.fromLTWH(dstCenter.left, 0, dstCenter.width, src.height);
-    switch (repeat.top) {
-      case NineGridRepeatType.repeat:
-        _composeRepeat(canvas, src, dst, paint);
-        break;
-      default:
-        _composeStretch(canvas, src, dst, paint);
-        canvas.drawImageRect(_image, src, dst, paint);
-        break;
+      // First draw all corners as an atlas (faster than drawing one by one)
+      canvas.drawAtlas(
+          _image,
+          <RSTransform>[
+            RSTransform(1.0, 0.0, 0.0, 0.0),
+            RSTransform(1.0, 0.0, dstCenter.right, 0.0),
+            RSTransform(1.0, 0.0, 0.0, dstCenter.bottom),
+            RSTransform(1.0, 0.0, dstCenter.right, dstCenter.bottom),
+          ],
+          <Rect>[
+            Rect.fromLTWH(0.0, 0.0, _grid.left, _grid.top),
+            Rect.fromLTWH(srcCenter.right, 0.0, _grid.right, _grid.top),
+            Rect.fromLTWH(0.0, srcCenter.bottom, _grid.left, _grid.bottom),
+            Rect.fromLTWH(srcCenter.right, srcCenter.bottom, _grid.right, _grid.bottom),
+          ],
+          null,
+          null,
+          null,
+          paint);
+
+      // Top
+      _composeRepeat(
+        canvas,
+        Rect.fromLTWH(srcCenter.left, 0, srcCenter.width, srcCenter.top),
+        Rect.fromLTWH(dstCenter.left, 0, dstCenter.width, dstCenter.top),
+        paint,
+        repeat.top == NineGridRepeatType.stretch,
+      );
+
+      // Left
+      _composeRepeat(
+        canvas,
+        Rect.fromLTWH(0, srcCenter.top, srcCenter.left, srcCenter.height),
+        Rect.fromLTWH(0, dstCenter.top, dstCenter.left, dstCenter.height),
+        paint,
+        false,
+        repeat.left == NineGridRepeatType.stretch,
+      );
+
+      // Center
+      _composeRepeat(
+        canvas,
+        srcCenter,
+        dstCenter,
+        paint,
+        repeat.centerH == NineGridRepeatType.stretch,
+        repeat.centerV == NineGridRepeatType.stretch,
+      );
+
+      // Right
+      _composeRepeat(
+        canvas,
+        Rect.fromLTWH(srcCenter.right, srcCenter.top, _grid.right, srcCenter.height),
+        Rect.fromLTWH(dstCenter.right, dstCenter.top, _grid.right, dstCenter.height),
+        paint,
+        false,
+        repeat.right == NineGridRepeatType.stretch,
+      );
+
+      // Bottom
+      _composeRepeat(
+        canvas,
+        Rect.fromLTWH(srcCenter.left, srcCenter.bottom, srcCenter.width, _grid.bottom),
+        Rect.fromLTWH(dstCenter.left, dstCenter.bottom, dstCenter.width, _grid.bottom),
+        paint,
+        repeat.bottom == NineGridRepeatType.stretch,
+      );
+
+      stopWatch.stop();
+      print('canvas operations took ${stopWatch.elapsedMicroseconds}');
     }
-
-    // Top right corner
-    src = Rect.fromLTWH(srcCenter.right, 0, _grid.right, _grid.top);
-    dst = Rect.fromLTWH(dstCenter.right, 0, max(0, size.x - dstCenter.right), dstCenter.top);
-    _composeStretch(canvas, src, dst, paint);
-
-    // Left
-    src = Rect.fromLTWH(0, srcCenter.top, srcCenter.left, srcCenter.height);
-    dst = Rect.fromLTWH(0, dstCenter.top, dstCenter.left, dstCenter.height);
-    switch (repeat.left) {
-      case NineGridRepeatType.repeat:
-        _composeRepeat(canvas, src, dst, paint);
-        break;
-      default:
-        _composeStretch(canvas, src, dst, paint);
-        break;
-    }
-
-    // Center
-    _composeRepeat(
-      canvas,
-      srcCenter,
-      dstCenter,
-      paint,
-      repeat.centerH == NineGridRepeatType.stretch,
-      repeat.centerV == NineGridRepeatType.stretch,
-    );
-
-    // Right
-    src = Rect.fromLTWH(srcCenter.right, srcCenter.top, _grid.right, srcCenter.height);
-    dst = Rect.fromLTWH(dstCenter.right, dstCenter.top, _grid.right, dstCenter.height);
-    switch (repeat.right) {
-      case NineGridRepeatType.repeat:
-        _composeRepeat(canvas, src, dst, paint);
-        break;
-      default:
-        _composeStretch(canvas, src, dst, paint);
-        break;
-    }
-
-    // Bottom left corner
-    src = Rect.fromLTWH(0, srcCenter.bottom, _grid.left, _grid.bottom);
-    dst = Rect.fromLTWH(0, dstCenter.bottom, _grid.left, _grid.bottom);
-    _composeStretch(canvas, src, dst, paint);
-
-    // Bottom
-    src = Rect.fromLTWH(_grid.left, srcCenter.bottom, srcCenter.width, _grid.bottom);
-    dst = Rect.fromLTWH(_grid.left, dstCenter.bottom, dstCenter.width, _grid.bottom);
-    switch (repeat.top) {
-      case NineGridRepeatType.repeat:
-        _composeRepeat(canvas, src, dst, paint);
-        break;
-      default:
-        _composeStretch(canvas, src, dst, paint);
-        canvas.drawImageRect(_image, src, dst, paint);
-        break;
-    }
-
-    // Bottom right corner
-    src = Rect.fromLTWH(srcCenter.right, srcCenter.bottom, _grid.right, _grid.bottom);
-    dst = Rect.fromLTWH(dstCenter.right, dstCenter.bottom, _grid.right, _grid.bottom);
-    _composeStretch(canvas, src, dst, paint);
 
     // Create image
     final picture = recorder.endRecording();
@@ -275,26 +300,68 @@ class NineGridComponent extends PositionComponent {
     });
   }
 
-  void _composeStretch(Canvas canvas, Rect src, Rect dst, Paint paint) {
-    if (src.isEmpty || dst.isEmpty) return;
-    canvas.drawImageRect(_image, src, dst, paint);
-  }
-
   void _composeRepeat(Canvas canvas, Rect src, Rect dst, Paint paint, [bool stretchX = false, bool stretchY = false]) {
     if (src.isEmpty || dst.isEmpty) return;
 
-    Rect start = Rect.fromLTWH(dst.left, dst.top, stretchX ? dst.width : src.width, stretchY ? dst.height : src.height);
-    Rect patch = Rect.fromLTWH(dst.left, dst.top, start.width, start.height);
-    int sx = (dst.width / patch.width).ceil();
-    int sy = (dst.height / patch.height).ceil();
-    for (int y = 0; y < sy; y++) {
-      for (int x = 0; x < sx; x++) {
+    Rect start = Rect.fromLTWH(
+      dst.left,
+      dst.top,
+      stretchX ? dst.width : src.width,
+      stretchY ? dst.height : src.height,
+    );
+    Rect patch = Rect.fromLTRB(
+      start.left,
+      start.top,
+      min(start.left + start.width, dst.right),
+      min(start.top + start.height, dst.bottom),
+    );
+
+    do {
+      do {
         canvas.drawImageRect(_image, src, patch, paint);
-        patch = dst.intersect(Rect.fromLTWH(patch.left + start.width, patch.top, start.width, start.height));
-      }
-      patch = dst.intersect(Rect.fromLTWH(start.left, patch.top + start.height, start.width, start.height));
-    }
+        patch = Rect.fromLTRB(
+          patch.left + start.width,
+          patch.top,
+          min(patch.right + start.width, dst.right),
+          patch.bottom,
+        );
+      } while (patch.left < dst.right);
+      patch = Rect.fromLTRB(
+        start.left,
+        patch.bottom,
+        min(start.left + start.width, dst.right),
+        min(patch.bottom + start.height, dst.bottom),
+      );
+    } while (patch.top < dst.bottom);
   }
+
+  /*
+  /// Faster because it uses a single drawAtlas call
+  /// However, currently does not support scaling. Note that the RSTransform only supports uniform scale :(
+  void _composeRepeat(Canvas canvas, Rect src, Rect dst, Paint paint, [bool stretchX = false, bool stretchY = false]) {
+    if (src.isEmpty || dst.isEmpty) return;
+
+    double sx = stretchX ? dst.width / src.width : 1.0;
+    double sy = stretchY ? dst.height / src.height : 1.0;
+    Rect patch = Rect.fromLTWH(
+      dst.left,
+      dst.top,
+      stretchX ? dst.width : src.width,
+      stretchY ? dst.height : src.height,
+    );
+    int tx = (dst.width / patch.width).ceil();
+    int ty = (dst.height / patch.height).ceil();
+    final transforms = <RSTransform>[];
+    final rects = <Rect>[];
+    for (y = 0; y < ty; y++) {
+      for (x = 0; x < tx; x++) {
+        transforms.add(RSTransform(1.0 * sx, 0.0 * sy, dst.left + x * patch.width, dst.top + y * patch.height));
+        rects.add(src);
+      }
+    }
+    canvas.drawAtlas(_image, transforms, rects, null, null, _sizeRect, paint);
+  }
+  */
 
   @mustCallSuper
   @override
@@ -308,7 +375,6 @@ class NineGridComponent extends PositionComponent {
     }
 
     if (doRender) {
-      print('render to $_sizeRect');
       canvas.drawImageRect(
         _output!,
         _sizeRect,
@@ -327,10 +393,12 @@ class BitmapNineGridComponent extends NineGridComponent with Snap {
     Vector2 size, {
     NineGridSize? grid,
     NineGridRepeat? repeat,
+    bool useSafeSize = false,
   }) : super(
           image,
           size,
           grid: grid,
           repeat: repeat,
+          useSafeSize: useSafeSize,
         );
 }
